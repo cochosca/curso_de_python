02ebb2ff26b1abe5d2a7b7f08db296f565db2517
cocho <josescappinibenitez@gmail.com>
2021-04-03 14:34:34 -0400

    Optimizacion del codigo con listas de comprencion

--- Aprendizaje/Proyectos/Frecuencias_estadistica.py
+++ Aprendizaje/Proyectos/Frecuencias_estadistica.py
@@ -1,9 +1,7 @@
 # copia los numeros en la lista
 numeros=[]
-variables=set()
 # extrae en un set todas la varibales del estudio
-for numero in numeros:
-    variables.add(numero)
+variable={numero for numero in numeros}
 # Determina la frecuencia de cada variable y la retorna
 for frecuencia_variable in variables:
     print(frecuencia_variable,numeros.count(frecuencia_variable),sep=": ")
